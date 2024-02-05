4ec356e855b67401ecfcfcb63aad047f55d631cb
caiourtiga <caiourtiga@hotmail.com>
2024-02-05 10:05:15 -0300

    adsasdadsa

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -33,10 +33,10 @@ const Body = () => {
           {t('description', { returnObjects: true }).map((item) => (
             <div className="rounded-4 m-2" key={item.id}>
               <div className="sectionbody">
-                <h2 className="m-2">{item.id}. {item.title}</h2>
+                <h2 className="m-1 mt-5">{item.id}. {item.title}</h2>
                 <p className="h5 py-2">&ensp;{item.text}</p>
-                <iframe className="text-center col-12" width="560" height="400" src={item.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className="asas">{item.text2} <a href={item.link2} target="_blank">{item.link2}</a></p>
+                <iframe className="text-center col-12 mt-2 my-1" width="560" height="400" src={item.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p className="asas mt-2">{item.text2} <a href={item.link2} target="_blank">{item.link2}</a></p>
                 <p className="asas">{item.text3} <a href={item.link3} target="_blank">{item.link3}</a></p>
               </div>
             </div>
